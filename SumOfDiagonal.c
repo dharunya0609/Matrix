25e7e9be89fee6abfa4d07f58ecb4f272f78cbd1
Dharunya <88583525+dharunya0609@users.noreply.github.com>
2022-09-01 21:20:26 +0530

    Create SumOfDiagonal.c

---
+++ SumOfDiagonal.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+ 
+int main()
+{
+ 	int i, j,n1,n2, a[10][10], sum;
+  
+ 	printf("\n Enter Number of rows and columns :  ");
+ 	scanf("%d %d", &n1, &n2);
+ 
+ 	printf("\n Enter the First Matrix Elements\n");
+ 	for(i=0;i<n1;i++)
+	{
+	 for(j=0;j<n2;j++)
+	 {
+	 	scanf("%d",&a[i][j]);
+	 }	
+	}
+	
+	
+	sum=0;
+	for(i=0;i<n1;i++)
+	{
+	 for(j=0;j<n2;j++)
+	 {
+	 	if(i==j)
+	 	{
+	 		sum+=a[i][j];
+		 }
+	 }	
+	}
+	
+	printf("Sum of diagonal is %d ",sum);
+	
+	
+  	
+ 	return 0;
+}
